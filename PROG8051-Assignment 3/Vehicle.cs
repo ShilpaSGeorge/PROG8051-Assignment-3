96386ede5ae04d2005d7786c935764190905ae49
Shilpa Sosa George <shilpasosageorge@gmail.com>
2024-03-20 12:54:34 -0400

    Add project files.

---
+++ PROG8051-Assignment 3/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG8051_Assignment_3
+{
+    class Vehicle
+    {
+        public string Model;
+        public string Manufacturer;
+        public int Year;
+        public double RentalPrice;
+
+        public string VehicleModel { get { return Model; } set { Model = value; }}
+        public string VehicleManufacturer { get { return Manufacturer; } set {  Manufacturer = value; }}
+        public int MadeYear { get { return Year; } set {  Year = value; }}
+        public double VehiclePrice { get { return RentalPrice; } set { RentalPrice = value; }}
+
+        public virtual void DisplayDetails()
+        {
+            Console.WriteLine($"Model : {VehicleModel}\n Manufacturer : {VehicleManufacturer}\n Year of Manufacture : {MadeYear}\n Price of Vehicle : {VehiclePrice}");
+        }
+
+    }
+}
